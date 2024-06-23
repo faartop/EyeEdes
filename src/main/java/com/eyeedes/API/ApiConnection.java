55c994eb143610e7169d790c50d3bb23fa9dbc12
faartop <132773949+faartop@users.noreply.github.com>
2024-06-23 02:30:58 -0300

    Divisião da classe em blocos para melhor legibilidade, adicionando tbm try-with-resources para gerenciar automaticamente o fechamento do buffer

--- src/main/java/com/eyeedes/API/ApiConnection.java
+++ src/main/java/com/eyeedes/API/ApiConnection.java
@@ -1,6 +1,7 @@
 package com.eyeedes.API;
 
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,78 +15,81 @@ public class ApiConnection {
 
     private static final Logger LOGGER = Logger.getLogger(ApiConnection.class.getName());
 
-    public static JSONObject apiCEP(String cep) {
-        String urlAPI = "https://viacep.com.br/ws/" + cep + "/json/";
+    public static JSONObject validaUrl(String cep) {
+        String apiUrl = "https://viacep.com.br/ws/" + cep + "/json/";
         JSONObject responseJson = null;
-        JSONObject filtroAPI = new JSONObject();
-        HttpURLConnection conn = null;
-        BufferedReader inputCEP = null;
 
         try {
-            URL url = new URL(urlAPI);
-            conn = (HttpURLConnection) url.openConnection();
+            responseJson = apiCEP(apiUrl);
+            return filtraResponse(responseJson);
+        } catch (MalformedURLException e) {
+            LOGGER.log(Level.SEVERE, "URL inválida! Verifique o CEP e tente novamente.", e);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Erro de I/O ocorreu ao tentar conectar com a API.", e);
+        }
 
+        return null;
+    }
+
+    public static JSONObject apiCEP(String apiUrl) throws IOException {
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(apiUrl);
+            conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setConnectTimeout(5000);
             conn.setReadTimeout(5000);
             conn.setRequestProperty("Accept", "application/json");
 
-            int retornoAPIcod = conn.getResponseCode();
+            int responseCode = conn.getResponseCode();
 
-            if (retornoAPIcod == 400) {
+            if (responseCode == 400) {
                 LOGGER.log(Level.WARNING, "CEP inválido, verifique se não há:\n- Espaços\n- Letras\n- Dígitos a mais");
                 return null;
-            } else if (retornoAPIcod != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Falha: Erro HTTP: " + retornoAPIcod);
+            } else if (responseCode != HttpURLConnection.HTTP_OK) {
+                throw new RuntimeException("Falha: Erro HTTP: " + responseCode);
             }
 
-            inputCEP = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder outputCEP = new StringBuilder();
-            String output;
-
-            while ((output = inputCEP.readLine()) != null) {
-                outputCEP.append(output);
-            }
-
-            responseJson = new JSONObject(outputCEP.toString());
-
-            if (responseJson.has("cep") && responseJson.has("logradouro") &&
-                    responseJson.has("bairro") && responseJson.has("localidade") &&
-                    responseJson.has("uf")) {
-
-                filtroAPI.put("cep", responseJson.getString("cep"));
-                filtroAPI.put("logradouro", responseJson.getString("logradouro"));
-                filtroAPI.put("bairro", responseJson.getString("bairro"));
-                filtroAPI.put("localidade", responseJson.getString("localidade"));
-                filtroAPI.put("uf", responseJson.getString("uf"));
-
-            } else {
-                LOGGER.log(Level.WARNING, "Resposta da API não contém todos os campos necessários.");
-                return null;
-            }
-
-        } catch (MalformedURLException e) {
-            LOGGER.log(Level.SEVERE, "URL inválida! Verifique o CEP e tente novamente.", e);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Erro de I/O ocorreu ao tentar conectar com a API.", e);
-            throw new RuntimeException(e);
-        } finally {
-            if (inputCEP != null) {
-                try {
-                    inputCEP.close();
-                } catch (IOException e) {
-                    LOGGER.log(Level.WARNING, "Erro ao fechar BufferedReader.", e);
+            try (BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = input.readLine()) != null) {
+                    response.append(line);
                 }
+                return new JSONObject(response.toString());
             }
+        } finally {
             if (conn != null) {
                 conn.disconnect();
             }
         }
-        return filtroAPI;
     }
 
-    public static void printCepData(String cep) { /*Usar para pegar o endereço pelo CEP*/
-        JSONObject resultado = apiCEP(cep);
+    private static JSONObject filtraResponse(JSONObject responseJson) {
+        if (responseJson == null) {
+            return null;
+        }
+
+        if (responseJson.has("cep") && responseJson.has("logradouro") &&
+                responseJson.has("bairro") && responseJson.has("localidade") &&
+                responseJson.has("uf")) {
+
+            JSONObject filteredJson = new JSONObject();
+            filteredJson.put("cep", responseJson.getString("cep"));
+            filteredJson.put("logradouro", responseJson.getString("logradouro"));
+            filteredJson.put("bairro", responseJson.getString("bairro"));
+            filteredJson.put("localidade", responseJson.getString("localidade"));
+            filteredJson.put("uf", responseJson.getString("uf"));
+
+            return filteredJson;
+        } else {
+            LOGGER.log(Level.WARNING, "Resposta da API não contém todos os campos necessários.");
+            return null;
+        }
+    }
+
+    public static void printCepData(String cep) {
+        JSONObject resultado = validaUrl(cep);
         if (resultado != null) {
             System.out.println("Dados do CEP:");
             System.out.println("CEP: " + resultado.optString("cep"));
@@ -97,4 +101,5 @@ public class ApiConnection {
             System.out.println("Não foi possível obter os dados do CEP ou os dados retornados são incompletos.");
         }
     }
+
 }
